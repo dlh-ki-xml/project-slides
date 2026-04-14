75045884dcaa6e2881fa68cf091632bf53020d72
Thomas Lampart <thomas@runner-kswz6skxu-project-61042842-concurrent-0>
2026-04-14 10:11:10 +0200

    Add first published presentation

---
+++ GenKI2-SemA-ZB/slides.md
@@ -0,0 +1,17 @@
+<a href="https://github.com/dlh-ki-xml">
+						<img src="images/logo-pdftoxml.svg" alt="Logo PDTtoXML" style="height: 100px; margin: 0 auto 4rem auto; background: transparent;" class="img-rounded">
+					</a>
+					<h2>Prüfungen einfach in Moodle transferieren</h2>
+					<h5>Zwischenbericht vom Do. 23. April 2026</h5>
+					<p>
+						<small><a href="mailto:thomas.lampart@students.ffhs.ch">Thomas Lampart  &#124;  <i class="fas fa-envelope-square"></i> thomas.lampart@students.ffhs.ch</a><br><a href="https://www.ffhs.ch">www.ffhs.ch</a>
+                        </p>
+                        <p>
+						<a href="https://www.ffhs.ch">
+						<img src="images/logo-ffhs.svg" alt="FFHS-Logo" style="height: 80px; margin: 0 auto 4rem auto; background: transparent;" class="demo-logo">
+						</a></small>
+					</p>
+---
+
+## Test
+
---
+++ shared/config.js
@@ -0,0 +1,45 @@
+Reveal.initialize({
+				controls: true,
+				progress: true,
+				center: true,
+				hash: true,
+				transition: 'convex',
+
+				animate: {
+  					autoplay: true
+				},
+
+				customcontrols: {
+    				controls: [
+      					{
+       						 id: 'toggle-overview',
+        					title: 'Overview',
+       						 icon: '<i class="fa fa-th"></i>',
+        					action: 'Reveal.toggleOverview();'
+      					},
+						{
+      						id: 'toggle-chalkboard',
+      						title: 'Chalkboard',
+      						icon: '<i class="fa fa-pen-square"></i>',
+     						action: 'RevealChalkboard.toggleChalkboard();'
+   						 },
+{
+     						 id: 'toggle-fullscreen',
+      						title: 'Fullscreen',
+      						icon: '<i class="fa fa-expand"></i>',
+      						action: 'document.fullscreenElement ? document.exitFullscreen() : document.documentElement.requestFullscreen();'
+  	  }
+   					 ]
+},
+anything: [
+  {
+    className: "anything",
+    initialize: function(container, options) {
+      container.innerHTML = "<p>" + options.text + "</p>";
+    }
+  }
+],
+				// Learn about plugins: https://revealjs.com/plugins/
+				
+				plugins: [RevealZoom, RevealNotes, RevealSearch, RevealMarkdown, RevealHighlight, RevealChalkboard, RevealCustomControls, RevealLoadContent, RevealAnimate, RevealChart, RevealAnything],
+			});
